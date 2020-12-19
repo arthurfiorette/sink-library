fe1bde650d2e45d99bc6df66a962423c18209e78
Hazork <arthur.fiorette@gmail.com>
2020-12-19 06:41:52 -0300

    Externalizado textos

--- src/com/github/hazork/sinkspigot/Sinks.java
+++ src/com/github/hazork/sinkspigot/Sinks.java
@@ -7,17 +7,19 @@ import com.google.common.collect.Maps;
 
 public final class Sinks {
 
+    private static final String registerError = "Attempt to register a plugin id that already exists: %s.\n Use: SinkPlugin#from to get the instance.";
+    private static final String fromError = "Attempt to get a non-existent instance";
+
     private static Map<Class<? extends SinkPlugin>, SinkPlugin> pluginMap = Maps.newHashMap();
 
     static <S extends SinkPlugin> void newInstance(S plugin) {
-	final String registerError = "Attempt to register a plugin id that already exists: %s.\n Use: SinkPlugin#from to get the instance.";
 	Class<? extends SinkPlugin> clazz = plugin.getClass();
 	Verify.verify(pluginMap.containsKey(clazz), registerError, clazz.getSimpleName());
 	pluginMap.put(clazz, plugin);
     }
 
     public static <T extends SinkPlugin> T from(Class<T> clazz) {
-	Verify.verify(pluginMap.containsKey(clazz), "Attempt to get a non-existent instance");
+	Verify.verify(pluginMap.containsKey(clazz), fromError);
 	return clazz.cast(pluginMap.get(clazz));
     }
 
