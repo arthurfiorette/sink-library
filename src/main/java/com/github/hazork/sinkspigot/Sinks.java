e8bab97760d223fd91e856dc76d7da1c6812b3a1
Hazork <arthur.fiorette@gmail.com>
2020-12-21 15:28:35 -0300

    v1.0.3

--- src/main/java/com/github/hazork/sinkspigot/Sinks.java
+++ src/main/java/com/github/hazork/sinkspigot/Sinks.java
@@ -5,9 +5,14 @@ import java.util.Map;
 import com.google.common.base.Verify;
 import com.google.common.collect.Maps;
 
+/**
+ * Internal library class for checking the construction of SinkPlugin instances
+ * 
+ * @author https://github.com/Hazork/sink-library/
+ */
 public final class Sinks {
 
-    public static final String VERSION = "v1.0.0";
+    public static final String VERSION = "v1.0.3";
 
     private static final String registerError = "Attempt to register a plugin id that already exists: %s.\n Use: SinkPlugin#from to get the instance.";
     private static final String fromError = "Attempt to get a non-existent instance";
@@ -20,6 +25,13 @@ public final class Sinks {
 	pluginMap.put(clazz, plugin);
     }
 
+    /**
+     * Returns the plugin instance from the T class
+     * 
+     * @param <T>   T extends {@link SinkPlugin}
+     * @param clazz the class to get the instance.
+     * @return the instance
+     */
     public static <T extends SinkPlugin> T from(Class<T> clazz) {
 	Verify.verify(pluginMap.containsKey(clazz), fromError);
 	return clazz.cast(pluginMap.get(clazz));
