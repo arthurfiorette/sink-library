f1300949f5e997ee29fe5b0dd46a08eceac8981c
ArthurFiorette <ArthurFiorette@users.noreply.github.com>
2021-06-27 12:35:20 +0000

    style(prettier): prettified code

--- src/main/java/com/github/arthurfiorette/sinklibrary/menu/item/BuildedStack.java
+++ src/main/java/com/github/arthurfiorette/sinklibrary/menu/item/BuildedStack.java
@@ -1,10 +1,9 @@
 package com.github.arthurfiorette.sinklibrary.menu.item;
 
-import org.bukkit.inventory.ItemStack;
-
 import com.github.arthurfiorette.sinklibrary.item.ItemBuilder;
 import com.github.arthurfiorette.sinklibrary.menu.BaseMenu;
 import com.github.arthurfiorette.sinklibrary.menu.listener.ClickListener;
+import org.bukkit.inventory.ItemStack;
 
 public class BuildedStack<T extends BaseMenu> implements MenuItem {
 
