0b4aafd04afaf106a76ac36b9e3e7816b86d3dc7
Hazork <arthur.fiorette@gmail.com>
2021-07-11 17:15:51 -0300

    feat: simple adapter for storages

---
+++ src/main/java/com/github/arthurfiorette/sinklibrary/data/storage/addons/IdentifiableAdapter.java
@@ -0,0 +1,19 @@
+package com.github.arthurfiorette.sinklibrary.data.storage.addons;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import com.github.arthurfiorette.sinklibrary.data.storage.Storage;
+import com.github.arthurfiorette.sinklibrary.interfaces.Identifiable;
+
+public interface IdentifiableAdapter<V extends Identifiable, R> extends Storage<UUID, V, R> {
+
+  default CompletableFuture<Void> save(V value) {
+    return this.save(value.getUniqueId(), value);
+  }
+
+  default void saveSync(final V value) {
+    this.saveSync(value.getUniqueId(), value);
+  }
+
+}
