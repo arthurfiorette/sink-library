6a35b3a8c669b6ba8219dcf553e4a0fdfd4cf702
Hazork <arthur.fiorette@gmail.com>
2021-09-06 14:51:08 -0300

    style: use @arthurfiorette/prettier-config

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+// https://github.com/ArthurFiorette/prettier-config
+
+// Globally installed, also installed on gh prettier action
+module.exports = require('@arthurfiorette/prettier-config')({
+  // Any options here will override @arthurfiorette/prettier-config defaults
+});
